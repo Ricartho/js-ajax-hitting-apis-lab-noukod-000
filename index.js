e7bf230833f054f6a78de00a55f6250e7eed7039
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 19:47:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function displayRepositories(){
+    const repos = JSON.parse(this.responseText);
+    const repoList = `<ul>${repos.map(r => '<li><strong>' + r.name +'</strong> by '+ r.owner.login + ' -<a href="https://github.com/'+r.full_name+'">Details</a>' + '</li>').join('')}</ul>`;
+   document.getElementById("repositories").innerHTML = repoList;
+}
+ 
+function getRepositories(){
+  const user = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load',displayRepositories);
+  req.open('GET','https://api.github.com/users/'+ user +'/repos');
+  req.send();
+  
+}
