3b923d373b62c8f44e28b3acd4f5c5729ab15117
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 20:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function getCommits(el){
   const username = el.dataset.user;
   const repository = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.open('GET','https://api.github.com/users/'+username+'/'+repository+'/commits');
+  req.open('GET','https://api.github.com/users/'+ username +'/'+ repository +'/commits');
   req.send();
 }
 
