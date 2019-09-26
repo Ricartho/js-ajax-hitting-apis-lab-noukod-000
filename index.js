576b3fcc54d7d95275701bd90abdbb8e5a8e3dc2
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 20:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+document.getElementById('details',function(event){
+  event.preventDefault();
+})
+function getCommits(el){
+  el.preventDefault();
+  const username = el.dataset.user;
+  const repository = el.dataset.repo
+  const req = new XMLHttpRequest();
+  req.open('GET','https://api.github.com/users/'+username+'/'+repository+'/commits');
+  req.send();
+}
 function displayRepositories(){
     const repos = JSON.parse(this.responseText);
     const repoList = `<ul>${repos.map(r => '<li><strong>' + r.name +'</strong> by '+ r.owner.login + ' -<a href="https://github.com/'+r.full_name+'" data-repo="'+r.name+'"data-user="'+r.owner.login+'"onclick="getCommits(this)" id="details">Details</a>' + '</li>').join('')}</ul>`;
@@ -13,14 +24,3 @@ function getRepositories(){
   
 }
 
-document.getElementById('details',function(event){
-  event.preventDefault();
-})
-function getCommits(el){
-  el.preventDefault();
-  const username = el.dataset.user;
-  const repository = el.dataset.repo
-  const req = new XMLHttpRequest();
-  req.open('GET','https://api.github.com/users/'+username+'/'+repository+'/commits');
-  req.send();
-}
